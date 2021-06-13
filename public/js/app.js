cbf6a695c6ba30990b57189b9bf259ede570340c
thanh <dangthanh3790@gmail.com>
2021-06-13 14:49:48 +0700

    'rss-v1'

--- public/js/app.js
+++ public/js/app.js
@@ -1,28 +1,70 @@
-console.log('abc')
+let url = 'https://vnexpress.net/rss/suc-khoe.rss'
 
-const FEED_URL = `https://vnexpress.net/rss/suc-khoe.rss`;
+feednami.load(url, function (result) {
+  if (result.error) {
+    console.log(result.error)
+  }
+  else {
+    let news = result.feed.entries.sort((a, b) => {
+      return new Date(b.pubDate) - new Date(a.pubDate);
+    }).slice(0, 20)
+    render(news)
+  }
+})
 
-fetch(FEED_URL, )
-  .then(response => response.text())
-  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
-  .then(data => {
-    console.log(data);
-    const items = data.querySelectorAll("item");
-    let html = ``;
-    html += `<h2>${data.querySelector("title").innerHTML}</h2>`;
-    html += `<p>${data.querySelector("description").innerHTML}</p>`;
-    html += `<div class="feeds">`;
-    items.forEach(el => {
-        html += `
-        <article>
-          <h3>
-            <a href="${el.querySelector("link").innerHTML}" target="_blank">
-              ${el.querySelector("title").innerHTML}
-            </a>
-          </h3>
-        </article>
-      `;
-    });
-    document.getElementById('content').insertAdjacentHTML("beforeend", html);
-  });
+function render(array) {
+  for (let news of array) {
+
+    const div = document.createElement('div')
+    const h3 = document.createElement('h3')
+    const inner = document.createElement('div')
+    const time = document.createElement('p')
+    const a = document.createElement('a')
+
+    div.classList.add('item', 'col-md-6', 'col-12')
+
+    h3.innerHTML = news.title
+    inner.innerHTML = news.description
+    time.innerHTML = new Date(news.pubDate)
+    a.innerText = 'More'
+    a.href = news.link
+
+    div.appendChild(h3)
+    div.appendChild(time)
+    div.appendChild(inner)
+    div.appendChild(a)
+    document.querySelector('.articles').append(div)
+
+    document.querySelectorAll('a').forEach(link => {
+      link.setAttribute('target', '_blank')
+    })
+  }
+}
+
+document.querySelector('#filterForm').addEventListener('submit',
+  function filterForm() {
+    const startDay = document.querySelector('#startDay').value;
+    const endDay = document.querySelector('#endDay').value;
+
+    if (startDay !== '' && endDay !== '') {
+      feednami.load(url, function (result) {
+        if (result.error) {
+          console.log(result.error);
+        }
+        else {
+          let newsFilter = result.feed.entries.filter((news) => {
+            return new Date(news.pubDate).getTime() > new Date(startDay).getTime() && new Date(news.pubDate).getTime() < new Date(endDay).getTime()
+          })
+          const news = newsFilter.sort((a, b) => {
+            return new Date(b.pubDate) - new Date(a.pubDate);
+          }).slice(0, 20);
+          document.querySelector('.articles').innerHTML = '';
+          render(news);
+        }
+      });
+    }
+    else {
+      alert('Please select day');
+    }
+});
 
